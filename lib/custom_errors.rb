1eafd550b51525d46faf90494c1d7848b3734cb5
Nathan Steed <steedn@outlook.com>
2020-08-17 08:29:02 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,22 +1,23 @@
 class Person
   attr_accessor :partner, :name
-
+ 
   def initialize(name)
     @name = name
   end
-
+ 
   def get_married(person)
     self.partner = person
     if person.class != Person
       raise PartnerError
     else
-    person.partner = self
+      person.partner = self
     end
   end
+ 
   class PartnerError < StandardError
   end
 end
-
+ 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
